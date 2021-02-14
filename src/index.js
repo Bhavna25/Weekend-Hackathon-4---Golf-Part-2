087c19a9cf293f875cd81a7e988478c1500418f4
Bhavna25 <63115179+Bhavna25@users.noreply.github.com>
2021-02-14 17:54:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
